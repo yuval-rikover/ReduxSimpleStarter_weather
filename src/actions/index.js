52ebf1538702502533aea06e9e6a9958297a0aec
yuval-rikover <yuval.rikover@kenshoo.com>
2018-02-13 18:18:18 +0200

    yoop

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,16 @@
 import Axios from "axios";
 
 const API_KEY = '202b26c03b8f576261de661dae8f0546';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-export const FETCH_WEATHER = 'FERCH_WEATHER';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather (city) {
+export function fetchWeather(city) {
 
-    const url = `${ROOT_URL}&q=${city},IL`; 
+    const url = `${ROOT_URL}&q=${city},IL`;
     const request = Axios.get(url);
 
-    console.log("Request: ", request)
+    console.log("Request: ", request);
 
     return {
         type: FETCH_WEATHER,
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function(state = null, action) {
-    console.log("Action recieved", action);
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function (state = [], action) {
+    console.log("Action received", action);
+    switch (action.type) {
+        case FETCH_WEATHER:
+            return [action.payload.data, ...state];
+    }
+
     return state;
 }
